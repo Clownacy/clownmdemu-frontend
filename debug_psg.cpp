464cf0c206d56200fe737aaa623e710b2bac4d81
Clownacy <Clownacy@users.noreply.github.com>
2023-02-17 00:04:50 +0000

    Make the PSG debug menu way less ugly.

--- debug_psg.cpp
+++ debug_psg.cpp
@@ -10,81 +10,102 @@ void Debug_PSG(bool *open, ClownMDEmu *clownmdemu, ImFont *monospace_font)
 	if (ImGui::Begin("PSG", open, ImGuiWindowFlags_AlwaysAutoResize))
 	{
 		// Latched command.
-		if (ImGui::TreeNodeEx("Latched Command", ImGuiTreeNodeFlags_DefaultOpen))
+		ImGui::SeparatorText("Latched Command");
+		if (ImGui::BeginTable("Latched Command", 2, ImGuiTableFlags_Borders))
 		{
-			ImGui::PushFont(monospace_font);
+			ImGui::TableSetupColumn("Channel");
+			ImGui::TableSetupColumn("Type");
+			ImGui::TableHeadersRow();
+
+			ImGui::TableNextColumn();
 
 			if (clownmdemu->state->psg.latched_command.channel == 3)
-				ImGui::TextUnformatted("Channel: Noise");
+				ImGui::TextUnformatted("Noise");
 			else
-				ImGui::Text("Channel: Tone #%" CC_PRIuFAST8, clownmdemu->state->psg.latched_command.channel + 1);
+				ImGui::Text("Tone %" CC_PRIuFAST8, clownmdemu->state->psg.latched_command.channel + 1);
 
-			ImGui::TextUnformatted(clownmdemu->state->psg.latched_command.is_volume_command ? "Type:    Attenuation" : "Type:    Frequency");
+			ImGui::TableNextColumn();
 
-			ImGui::PopFont();
+			ImGui::TextUnformatted(clownmdemu->state->psg.latched_command.is_volume_command ? "Attenuation" : "Frequency");
 
-			ImGui::TreePop();
+			ImGui::EndTable();
 		}
 
 		// Channels.
-		if (ImGui::TreeNodeEx("Channels", ImGuiTreeNodeFlags_DefaultOpen))
-		{
-			const cc_u32f psg_clock = (clownmdemu->configuration->general.tv_standard == CLOWNMDEMU_TV_STANDARD_PAL ? CLOWNMDEMU_MASTER_CLOCK_PAL : CLOWNMDEMU_MASTER_CLOCK_NTSC) / 15 / 16;
+		const cc_u32f psg_clock = (clownmdemu->configuration->general.tv_standard == CLOWNMDEMU_TV_STANDARD_PAL ? CLOWNMDEMU_MASTER_CLOCK_PAL : CLOWNMDEMU_MASTER_CLOCK_NTSC) / 15 / 16;
 
-			// Tone channels.
+		// Tone channels.
+		ImGui::SeparatorText("Tone Channels");
+		if (ImGui::BeginTable("Tone Channels", 3, ImGuiTableFlags_Borders))
+		{
+			ImGui::TableSetupColumn("Channel");
+			ImGui::TableSetupColumn("Frequency");
+			ImGui::TableSetupColumn("Attentuation");
+			ImGui::TableHeadersRow();
+
 			for (cc_u8f i = 0; i < CC_COUNT_OF(clownmdemu->state->psg.tones); ++i)
 			{
-				char string_buffer[sizeof("Tone 0")];
-				SDL_snprintf(string_buffer, sizeof(string_buffer), "Tone %" CC_PRIuFAST8, i + 1);
-				if (ImGui::TreeNodeEx(string_buffer, ImGuiTreeNodeFlags_DefaultOpen))
-				{
-					ImGui::PushFont(monospace_font);
+				ImGui::TableNextColumn();
+				ImGui::Text("Tone %" CC_PRIuFAST8, i + 1);
 
-					ImGui::Text("Frequency: 0x%03" CC_PRIXFAST16 " (%6" CC_PRIuFAST32 "Hz)", clownmdemu->state->psg.tones[i].countdown_master, psg_clock / (clownmdemu->state->psg.tones[i].countdown_master + 1) / 2);
-
-					if (clownmdemu->state->psg.tones[i].attenuation == 15)
-						ImGui::TextUnformatted("Attenuation: 0xF (Mute)");
-					else
-						ImGui::Text("Attenuation: 0x%" CC_PRIXFAST8 " (%2" CC_PRIuFAST8 "db)", clownmdemu->state->psg.tones[i].attenuation, clownmdemu->state->psg.tones[i].attenuation * 2);
-
-					ImGui::PopFont();
-
-					ImGui::TreePop();
-				}
-			}
-
-			// Noise channel.
-			if (ImGui::TreeNodeEx("Noise", ImGuiTreeNodeFlags_DefaultOpen))
-			{
 				ImGui::PushFont(monospace_font);
 
-				switch (clownmdemu->state->psg.noise.type)
-				{
-					case PSG_NOISE_TYPE_PERIODIC:
-						ImGui::TextUnformatted("Type: Periodic Noise");
-						break;
+				ImGui::TableNextColumn();
+				ImGui::Text("0x%03" CC_PRIXFAST16 " (%6" CC_PRIuFAST32 "Hz)", clownmdemu->state->psg.tones[i].countdown_master, psg_clock / (clownmdemu->state->psg.tones[i].countdown_master + 1) / 2);
 
-					case PSG_NOISE_TYPE_WHITE:
-						ImGui::TextUnformatted("Type: White Noise");
-						break;
-				}
-
-				if (clownmdemu->state->psg.noise.frequency_mode == 3)
-					ImGui::TextUnformatted("Frequency Mode: Tone #3");
+				ImGui::TableNextColumn();
+				if (clownmdemu->state->psg.tones[i].attenuation == 15)
+					ImGui::TextUnformatted("0xF (Mute)");
 				else
-					ImGui::Text("Frequency Mode: %" CC_PRIdFAST8 " (%4" CC_PRIuFAST32 "Hz)", clownmdemu->state->psg.noise.frequency_mode, psg_clock / (0x10 << clownmdemu->state->psg.noise.frequency_mode) / 2);
-
-				if (clownmdemu->state->psg.noise.attenuation == 15)
-					ImGui::TextUnformatted("Attenuation:  0xF (Mute)");
-				else
-					ImGui::Text("Attenuation:  0x%" CC_PRIXFAST8 " (%2" CC_PRIuFAST8 "db)", clownmdemu->state->psg.noise.attenuation, clownmdemu->state->psg.noise.attenuation * 2);
+					ImGui::Text("0x%" CC_PRIXFAST8 " (%2" CC_PRIuFAST8 "db)", clownmdemu->state->psg.tones[i].attenuation, clownmdemu->state->psg.tones[i].attenuation * 2);
 
 				ImGui::PopFont();
-
-				ImGui::TreePop();
 			}
 
-			ImGui::TreePop();
+			ImGui::EndTable();
+		}
+
+		// Noise channel.
+		ImGui::SeparatorText("Noise Channel");
+		if (ImGui::BeginTable("Noise Channel", 3, ImGuiTableFlags_Borders))
+		{
+			ImGui::TableSetupColumn("Noise Type");
+			ImGui::TableSetupColumn("Frequency Mode");
+			ImGui::TableSetupColumn("Attentuation");
+			ImGui::TableHeadersRow();
+
+			ImGui::TableNextColumn();
+
+			switch (clownmdemu->state->psg.noise.type)
+			{
+				case PSG_NOISE_TYPE_PERIODIC:
+					ImGui::TextUnformatted("Periodic");
+					break;
+
+				case PSG_NOISE_TYPE_WHITE:
+					ImGui::TextUnformatted("White");
+					break;
+			}
+
+			ImGui::PushFont(monospace_font);
+
+			ImGui::TableNextColumn();
+
+			if (clownmdemu->state->psg.noise.frequency_mode == 3)
+				ImGui::TextUnformatted("Tone 3");
+			else
+				ImGui::Text("%" CC_PRIdFAST8 " (%4" CC_PRIuFAST32 "Hz)", clownmdemu->state->psg.noise.frequency_mode, psg_clock / (0x10 << clownmdemu->state->psg.noise.frequency_mode) / 2);
+
+			ImGui::TableNextColumn();
+
+			if (clownmdemu->state->psg.noise.attenuation == 15)
+				ImGui::TextUnformatted("0xF (Mute)");
+			else
+				ImGui::Text("0x%" CC_PRIXFAST8 " (%2" CC_PRIuFAST8 "db)", clownmdemu->state->psg.noise.attenuation, clownmdemu->state->psg.noise.attenuation * 2);
+
+			ImGui::PopFont();
+
+			ImGui::EndTable();
 		}
 
 		ImGui::End();
