ce9c9aeeb96950e18f82e5c6eb3c280e2eacfd46
Clownacy <Clownacy@users.noreply.github.com>
2023-11-28 08:31:58 +0000

    Emscripten: Make configuration persistent.
    Now your settings are saved across uses of the emulator!

--- main.cpp
+++ main.cpp
@@ -2,15 +2,39 @@
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
+#include <emscripten/html5.h>
 #endif
 
 #include "frontend.h"
 
-int main(const int argc, char** const argv)
-{
 #ifdef __EMSCRIPTEN__
-	if (Frontend::Initialise(argc, argv))
+static void Terminate()
+{
+	Frontend::Deinitialise();
+	emscripten_cancel_main_loop();
+
+	// Deinitialise persistent storage.
+	EM_ASM({
+		FS.syncfs(false, function (err) {
+			Module._StorageLoaded();
+		});
+	}, 0);
+}
+
+static int EventFilter(void* /*const userdata*/, SDL_Event* const event)
+{
+	if (event->type == SDL_APP_TERMINATING)
+		Terminate();
+
+	return 0;
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE void StorageLoaded()
+{
+	if (Frontend::Initialise(0, nullptr))
 	{
+		SDL_AddEventWatch(EventFilter, nullptr);
+
 		const auto callback = []()
 		{
 			SDL_Event event;
@@ -20,15 +44,31 @@ int main(const int argc, char** const argv)
 			Frontend::Update();
 
 			if (Frontend::WantsToQuit())
-			{
-				Frontend::Deinitialise();
-				emscripten_cancel_main_loop();
-			}
+				Terminate();
 		};
 
 		// TODO: 50FPS (PAL)
 		emscripten_set_main_loop(callback, 60, 0);
 	}
+}
+#endif
+
+int main(const int argc, char** const argv)
+{
+#ifdef __EMSCRIPTEN__
+	static_cast<void>(argc);
+	static_cast<void>(argv);
+
+	// Initialise persistent storage.
+	EM_ASM({
+		FS.mkdir('/clownmdemu-frontend');
+		FS.mount(IDBFS, {}, '/clownmdemu-frontend');
+		FS.chdir('/clownmdemu-frontend');
+
+		FS.syncfs(true, function (err) {
+			Module._StorageLoaded();
+		});
+	}, 0);
 #else
 	if (Frontend::Initialise(argc, argv))
 	{
