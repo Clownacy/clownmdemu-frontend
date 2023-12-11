299dca1f03d20eed69873a8d55c2b33f534ba816
Clownacy <Clownacy@users.noreply.github.com>
2023-12-11 09:41:30 +0000

    Format some code.

--- sdl-wrapper.h
+++ sdl-wrapper.h
@@ -15,10 +15,10 @@ using NAME = std::unique_ptr<TYPE, NAME##Deleter>
 
 namespace SDL {
 
-SDL_WRAPPER_MAKE_RAII_POINTER(Window, SDL_Window, SDL_DestroyWindow);
+SDL_WRAPPER_MAKE_RAII_POINTER(Window,   SDL_Window,   SDL_DestroyWindow  );
 SDL_WRAPPER_MAKE_RAII_POINTER(Renderer, SDL_Renderer, SDL_DestroyRenderer);
-SDL_WRAPPER_MAKE_RAII_POINTER(Texture, SDL_Texture, SDL_DestroyTexture);
-SDL_WRAPPER_MAKE_RAII_POINTER(RWops, SDL_RWops, SDL_RWclose);
+SDL_WRAPPER_MAKE_RAII_POINTER(Texture,  SDL_Texture,  SDL_DestroyTexture );
+SDL_WRAPPER_MAKE_RAII_POINTER(RWops,    SDL_RWops,    SDL_RWclose        );
 
 }
 
