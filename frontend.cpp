Clownacy/clownmdemu-frontend

commit 683ef22810facd4222b0e93910a2c7bd80ef8a76

Switch to SDL2's way of enabling high-DPI support on Windows.
Less platform-specific code for me to maintain, hooray!

frontend.cpp
@@ -913,6 +913,9 @@ bool Frontend::Initialise(const int argc, char** const argv, const FrameRateCall
 {
 	frame_rate_callback = frame_rate_callback_param;
 
+	// Enable high-DPI support on Windows because SDL2 is bad at being a platform abstraction library
+	SDL_SetHint(SDL_HINT_WINDOWS_DPI_SCALING, "1");
+
 	// Initialise SDL2
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER) < 0)
 	{
@@ -1000,13 +1003,7 @@ bool Frontend::Initialise(const int argc, char** const argv, const FrameRateCall
 		{
 			// Resize the window so that there's room for the menu bar.
 			const float menu_bar_size = static_cast<float>(font_size) + style.FramePadding.y * 2.0f; // An inlined ImGui::GetFrameHeight that actually works
-		#ifdef _WIN32
-			// SDL2 does not have a proper high-DPI mechanism on Windows, so the window needs to be scaled by the DPI manually.
-			const float window_size_scale = (SDL_GetWindowFlags(window->GetSDLWindow()) & SDL_WINDOW_ALLOW_HIGHDPI) != 0 ? 1.0f : dpi_scale;
-		#else
-			const float window_size_scale = 1.0f;
-		#endif
-			SDL_SetWindowSize(window->GetSDLWindow(), static_cast<int>(INITIAL_WINDOW_WIDTH * window_size_scale), static_cast<int>(INITIAL_WINDOW_HEIGHT * window_size_scale + menu_bar_size));
+			SDL_SetWindowSize(window->GetSDLWindow(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT + menu_bar_size);
 		}
 
 		// Setup Platform/Renderer backends

CMakeLists.txt
@@ -66,13 +66,6 @@ add_executable(clownmdemu-frontend WIN32
 set_property(DIRECTORY . PROPERTY VS_STARTUP_PROJECT clownmdemu-frontend)
 
 if(WIN32)
-	# Link a manifest file that enables Hi-DPI support
-	target_sources(clownmdemu-frontend PRIVATE "clownmdemu-frontend.rc")
-
-	if (MSVC)
-		target_sources(clownmdemu-frontend PRIVATE "clownmdemu-frontend.manifest")
-	endif()
-
 	# Enable Unicode support so that non-ANSI file paths work
 	target_compile_definitions(clownmdemu-frontend PRIVATE UNICODE)
 endif()

clownmdemu-frontend.manifest
@@ -1,9 +1,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-  <application xmlns="urn:schemas-microsoft-com:asm.v3">
-    <windowsSettings>
-      <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true/pm</dpiAware>
-      <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">permonitorv2,permonitor</dpiAwareness>
-    </windowsSettings>
-  </application>
-</assembly>

clownmdemu-frontend.rc
@@ -1,7 +1,0 @@
-#include "winuser.rh"
-
-IDI_ICON1 ICON "assets/icon.ico"
-
-#ifdef __MINGW32__
-CREATEPROCESS_MANIFEST_RESOURCE_ID RT_MANIFEST "clownmdemu-frontend.manifest"
-#endif

window.cpp
@@ -8,22 +8,11 @@
 float Window::GetDPIScale() const
 {
 	// TODO: Make the DPI scale two-dimensional.
-	float dpi_scale;
-
-#ifdef _WIN32
-	// Windows needs silly custom bollocks. Maybe I should just use WinAPI.
-	dpi_scale = 1.0f;
-
-	float ddpi;
-	if (SDL_GetDisplayDPI(SDL_GetWindowDisplayIndex(GetSDLWindow()), &ddpi, nullptr, nullptr) == 0)
-		dpi_scale = ddpi / 96.0f;
-#else
 	int window_width, renderer_width;
 	SDL_GetWindowSize(GetSDLWindow(), &window_width, nullptr);
 	SDL_GetRendererOutputSize(GetRenderer(), &renderer_width, nullptr);
 
-	dpi_scale = static_cast<float>(renderer_width) / std::max(1, window_width); // Prevent a division by 0.
-#endif
+	const float dpi_scale = static_cast<float>(renderer_width) / std::max(1, window_width); // Prevent a division by 0.
 
 	// Prevent any insanity if we somehow get bad values.
 	return std::max(1.0f, dpi_scale);
@@ -31,14 +20,7 @@ float Window::GetDPIScale() const
 
 static SDL_Window* CreateWindow(const char* const window_title, const int window_width, const int window_height)
 {
-	SDL_Window* const window = SDL_CreateWindow(window_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN
-#ifndef _WIN32
-		// This currently does nothing on Windows, so we use our own custom implementation.
-		// However, in case this ever does do something in the future, avoid using it on Windows.
-		// We wouldn't want the two to clash.
-		| SDL_WINDOW_ALLOW_HIGHDPI
-#endif
-	);
+	SDL_Window* const window = SDL_CreateWindow(window_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_ALLOW_HIGHDPI);
 
 	if (window == nullptr)
 		throw std::runtime_error(std::string("SDL_CreateWindow failed with the following message - '") + SDL_GetError() + "'");

commit 3e5065299fcca43ba4456afff289eff86f195640

Fix window size being slightly wrong on high-DPI displays.
Was broken in 683ef22810facd4222b0e93910a2c7bd80ef8a76.

frontend.cpp
@@ -189,10 +189,12 @@ static bool emulator_on, emulator_running;
 // Fonts //
 ///////////
 
+static constexpr float UNSCALED_FONT_SIZE = 16.0f;
+
 static unsigned int CalculateFontSize()
 {
 	// Note that we are purposefully flooring, as Dear ImGui's docs recommend.
-	return static_cast<unsigned int>(16.0f * dpi_scale);
+	return static_cast<unsigned int>(UNSCALED_FONT_SIZE * dpi_scale);
 }
 
 static void ReloadFonts(const unsigned int font_size)
@@ -1016,8 +1018,6 @@ bool Frontend::Initialise(const int argc, char** const argv, const FrameRateCall
 		// Apply DPI scale.
 		style.ScaleAllSizes(dpi_scale);
 
-		const unsigned int font_size = CalculateFontSize();
-
 		// We shouldn't resize the window if something is overriding its size.
 		// This is needed for the Emscripen build to work correctly in a full-window HTML canvas.
 		int window_width, window_height;
@@ -1026,7 +1026,7 @@ bool Frontend::Initialise(const int argc, char** const argv, const FrameRateCall
 		if (window_width == INITIAL_WINDOW_WIDTH && window_height == INITIAL_WINDOW_HEIGHT)
 		{
 			// Resize the window so that there's room for the menu bar.
-			const float menu_bar_size = static_cast<float>(font_size) + style.FramePadding.y * 2.0f; // An inlined ImGui::GetFrameHeight that actually works
+			const float menu_bar_size = UNSCALED_FONT_SIZE + style.FramePadding.y * 2.0f; // An inlined ImGui::GetFrameHeight that actually works
 			SDL_SetWindowSize(window->GetSDLWindow(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT + menu_bar_size);
 		}
 
@@ -1035,7 +1035,7 @@ bool Frontend::Initialise(const int argc, char** const argv, const FrameRateCall
 		ImGui_ImplSDLRenderer2_Init(window->GetRenderer());
 
 		// Load fonts
-		ReloadFonts(font_size);
+		ReloadFonts(CalculateFontSize());
 
 		// If the user passed the path to the software on the command line, then load it here, automatically.
 		if (argc > 1)