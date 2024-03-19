b657bccb2c9cfc1779e4d6a34307cf5bad52484a
Clownacy <Clownacy@users.noreply.github.com>
2024-03-19 19:33:48 +0000

    Rearrange the PCM register debug menu.
    Now tidier and easier to understand.

--- debug-pcm.cpp
+++ debug-pcm.cpp
@@ -5,17 +5,16 @@ void DebugPCM::Display(bool &open)
 	if (ImGui::Begin("PCM Registers", &open, ImGuiWindowFlags_AlwaysAutoResize))
 	{
 		ImGui::SeparatorText("Channels");
-		if (ImGui::BeginTable("Channels", 9, ImGuiTableFlags_Borders))
+		if (ImGui::BeginTable("Channels", 8, ImGuiTableFlags_Borders))
 		{
 			ImGui::TableSetupColumn("Channel");
-			ImGui::TableSetupColumn("Address");
-			ImGui::TableSetupColumn("Address (Whole)");
-			ImGui::TableSetupColumn("Disabled");
+			ImGui::TableSetupColumn("Enabled");
 			ImGui::TableSetupColumn("Frequency");
-			ImGui::TableSetupColumn("Loop Address");
-			ImGui::TableSetupColumn("Panning");
-			ImGui::TableSetupColumn("Start Address");
 			ImGui::TableSetupColumn("Volume");
+			ImGui::TableSetupColumn("Panning");
+			ImGui::TableSetupColumn("Address");
+			ImGui::TableSetupColumn("Start Address");
+			ImGui::TableSetupColumn("Loop Address");
 			ImGui::TableHeadersRow();
 
 			const PCM_State &pcm = emulator.CurrentState().clownmdemu.mega_cd.pcm;
@@ -27,31 +26,28 @@ void DebugPCM::Display(bool &open)
 				ImGui::TableNextColumn();
 				ImGui::Text("%" CC_PRIuFAST8, static_cast<cc_u8f>(i + 1));
 
+				ImGui::TableNextColumn();
+				ImGui::TextUnformatted(channel.disabled ? "No" : "Yes");
+
 				ImGui::PushFont(monospace_font);
 
 				ImGui::TableNextColumn();
-				ImGui::Text("0x%" CC_PRIXFAST32, static_cast<cc_u32f>(channel.address >> 11));
+				ImGui::Text("0x%04" CC_PRIXLEAST16 " (%5" CC_PRIuFAST32 "Hz)", channel.frequency, static_cast<cc_u32f>(channel.frequency * CLOWNMDEMU_PCM_SAMPLE_RATE / 0x800));
 
 				ImGui::TableNextColumn();
-				ImGui::Text("0x%" CC_PRIXLEAST32, channel.address);
+				ImGui::Text("0x%02" CC_PRIXLEAST8, channel.volume);
 
 				ImGui::TableNextColumn();
-				ImGui::TextUnformatted(channel.disabled ? "Yes" : "No");
+				ImGui::Text("0x%02" CC_PRIXFAST8, static_cast<cc_u8f>((channel.panning[0] << 0) | (channel.panning[1] << 4)));
 
 				ImGui::TableNextColumn();
-				ImGui::Text("0x%04" CC_PRIXLEAST16 " (%" CC_PRIuFAST32 "Hz)", channel.frequency, static_cast<cc_u32f>(channel.frequency * CLOWNMDEMU_PCM_SAMPLE_RATE / 0x800));
+				ImGui::Text("0x%04" CC_PRIXFAST32 ".%03" CC_PRIXFAST32, static_cast<cc_u32f>(channel.address / (1 << 11)), static_cast<cc_u32f>((channel.address % (1 << 11)) * 2));
 
 				ImGui::TableNextColumn();
-				ImGui::Text("0x%" CC_PRIXLEAST16, channel.loop_address);
+				ImGui::Text("0x%02" CC_PRIXLEAST8 "00", channel.start_address);
 
 				ImGui::TableNextColumn();
-				ImGui::Text("0x%" CC_PRIXFAST8, static_cast<cc_u8f>((channel.panning[0] << 0) | (channel.panning[1] << 4)));
-
-				ImGui::TableNextColumn();
-				ImGui::Text("0x%" CC_PRIXLEAST8, channel.start_address);
-
-				ImGui::TableNextColumn();
-				ImGui::Text("0x%" CC_PRIXLEAST8, channel.volume);
+				ImGui::Text("0x%04" CC_PRIXLEAST16, channel.loop_address);
 
 				ImGui::PopFont();
 			}
