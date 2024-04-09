33e846f9d9a5ceacf42d6dbbcf1858024de53436
Clownacy <Clownacy@users.noreply.github.com>
2024-04-09 03:43:47 +0100

    Overhaul how icons work and make the program installable.

---
+++ assets/icon/generate.sh
@@ -0,0 +1,11 @@
+magick icon-master.png -resize 512x512 icon-512.png
+magick icon-master.png -resize 256x256 icon-256.png
+magick icon-master.png -resize 128x128 icon-128.png
+magick icon-master.png -resize 64x64 icon-64.png
+magick icon-master.png -resize 48x48 icon-48.png
+magick icon-master.png -resize 40x40 icon-40.png
+#magick icon-master.png -resize 32x32 icon-32.png
+#magick icon-master.png -resize 24x24 icon-24.png
+#magick icon-master.png -resize 20x20 icon-20.png
+#magick icon-master.png -resize 16x16 icon-16.png
+magick convert icon-256.png icon-64.png icon-48.png icon-40.png icon-32.png icon-24.png icon-20.png icon-16.png icon.ico
