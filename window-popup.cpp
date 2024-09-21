242a842514207f2b66df0f3c0568441603df9ae0
Clownacy <Clownacy@users.noreply.github.com>
2024-09-21 07:27:58 +0100

    Allow popup windows to be regular Dear ImGui windows.
    I may add a setting for this.

--- window-popup.cpp
+++ window-popup.cpp
@@ -1,23 +1,48 @@
 #include "window-popup.h"
 
+WindowPopup::WindowPopup(DebugLog &debug_log, const char* const window_title, const int window_width, const int window_height, Window* const parent_window)
+	: title(window_title)
+	, width(window_width)
+	, height(window_height)
+	, parent_window(parent_window)
+{
+	if (parent_window == nullptr)
+	{
+		window.emplace(debug_log, window_title, window_width, window_height);
+		SDL_ShowWindow(window->GetSDLWindow());
+	}
+}
+
 bool WindowPopup::Begin()
 {
-	StartDearImGuiFrame();
+	ImGuiWindowFlags window_flags = 0;
 
-	const ImGuiViewport *viewport = ImGui::GetMainViewport();
+	if (parent_window == nullptr)
+	{
+		window->StartDearImGuiFrame();
 
-	// Maximise the Dear ImGui window.
-	ImGui::SetNextWindowPos(viewport->WorkPos);
-	ImGui::SetNextWindowSize(viewport->WorkSize);
+		const ImGuiViewport *viewport = ImGui::GetMainViewport();
 
-	ImGui::PushID(this);
-	return ImGui::Begin("Popup Window", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings);
+		// Maximise the Dear ImGui window.
+		ImGui::SetNextWindowPos(viewport->WorkPos);
+		ImGui::SetNextWindowSize(viewport->WorkSize);
+
+		window_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings;
+	}
+	else
+	{
+		ImGui::SetNextWindowSize(ImVec2(width * parent_window->GetDPIScale(), height * parent_window->GetDPIScale()), ImGuiCond_FirstUseEver);
+	}
+
+	//ImGui::PushID(this);
+	return ImGui::Begin(title.c_str(), nullptr, window_flags);
 }
 
 void WindowPopup::End()
 {
 	ImGui::End();
-	ImGui::PopID();
+	//ImGui::PopID();
 
-	FinishDearImGuiFrame();
+	if (parent_window == nullptr)
+		window->FinishDearImGuiFrame();
 }
--- window-popup.h
+++ window-popup.h
@@ -1,16 +1,21 @@
 #ifndef WINDOW_POPUP_H
 #define WINDOW_POPUP_H
 
+#include <optional>
+#include <string>
+
 #include "window-with-dear-imgui.h"
 
-class WindowPopup : private WindowWithDearImGui
+class WindowPopup
 {
+private:
+	std::optional<WindowWithDearImGui> window;
+	std::string title;
+	int width, height;
+	Window *parent_window;
+
 public:
-	WindowPopup(DebugLog &debug_log, const char *window_title, int window_width, int window_height)
-		: WindowWithDearImGui(debug_log, window_title, window_width, window_height)
-	{
-		SDL_ShowWindow(GetSDLWindow());
-	}
+	WindowPopup(DebugLog &debug_log, const char *window_title, int window_width, int window_height, Window *parent_window = nullptr);
 
 	bool Begin();
 	void End();
