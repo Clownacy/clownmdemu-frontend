becaeea4c104d43c4beb9fcaeb0bdfef4695a063
Clownacy <Clownacy@users.noreply.github.com>
2025-01-31 23:35:01 +0000

    Stop using a deprecated ImageMagick command.

--- assets/icon/generate.sh
+++ assets/icon/generate.sh
@@ -8,4 +8,4 @@ magick icon-master.png -resize 40x40 icon-40.png
 #magick icon-master.png -resize 24x24 icon-24.png
 #magick icon-master.png -resize 20x20 icon-20.png
 #magick icon-master.png -resize 16x16 icon-16.png
-magick convert icon-256.png icon-64.png icon-48.png icon-40.png icon-32.png icon-24.png icon-20.png icon-16.png icon.ico
+magick icon-256.png icon-64.png icon-48.png icon-40.png icon-32.png icon-24.png icon-20.png icon-16.png icon.ico
