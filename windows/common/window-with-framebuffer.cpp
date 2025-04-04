7584951f27c3db5ac05da6fab53b70c35d330abd
Clownacy <Clownacy@users.noreply.github.com>
2025-04-04 23:57:46 +0100

    Actually, PIXELART probably won't be supported until SDL 3.4.
    Semver, bleh.

--- windows/common/window-with-framebuffer.cpp
+++ windows/common/window-with-framebuffer.cpp
@@ -9,7 +9,7 @@ SDL::Texture WindowWithFramebuffer::CreateFramebufferTexture(SDL::Renderer &rend
 	auto texture = SDL::CreateTexture(renderer, SDL_TEXTUREACCESS_STREAMING, framebuffer_width, framebuffer_height, SDL_SCALEMODE_NEAREST);
 
 	// Try to use SDL3's fancy new 'pixel-art' scaling mode.
-	// The cast is to support older versions of SDL3 (pre-3.2.12).
+	// The cast is to support older versions of SDL3 (pre-3.4).
 	SDL_SetTextureScaleMode(texture, static_cast<SDL_ScaleMode>(2)/*SDL_SCALEMODE_PIXELART*/);
 
 	return texture;
