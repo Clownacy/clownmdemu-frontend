909d7cec3618bcc769ee7d27c55721a2ee333295
Clownacy <Clownacy@users.noreply.github.com>
2025-06-16 20:30:37 +0100

    Make the About dialog show a proper version.

--- qt/about.cpp
+++ qt/about.cpp
@@ -8,4 +8,6 @@ About::About(QWidget *parent) :
 	ui.setupUi(this);
 
 	connect(ui.buttonBox, &QDialogButtonBox::rejected, this, &About::close);
+
+	ui.label->setText(ui.label->text().replace("[VERSION]", "v1.5"));
 }
