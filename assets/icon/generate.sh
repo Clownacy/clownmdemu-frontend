1031025a148fb189261af1e5b589033be06241e6
Clownacy <Clownacy@users.noreply.github.com>
2025-06-22 22:15:39 +0100

    Qt: Add icon to the About menu.

--- assets/icon/generate.sh
+++ assets/icon/generate.sh
@@ -1,11 +1,12 @@
-magick icon-master.png -resize 512x512 icon-512.png
-magick icon-master.png -resize 256x256 icon-256.png
-magick icon-master.png -resize 128x128 icon-128.png
-magick icon-master.png -resize 64x64 icon-64.png
-magick icon-master.png -resize 48x48 icon-48.png
-magick icon-master.png -resize 40x40 icon-40.png
-#magick icon-master.png -resize 32x32 icon-32.png
-#magick icon-master.png -resize 24x24 icon-24.png
-#magick icon-master.png -resize 20x20 icon-20.png
-#magick icon-master.png -resize 16x16 icon-16.png
+magick icon-master.png -thumbnail 512x512 icon-qt.png
+magick icon-master.png -thumbnail 512x512 -background transparent -gravity center -extent 512x512 icon-512.png
+magick icon-master.png -thumbnail 256x256 -background transparent -gravity center -extent 256x256 icon-256.png
+magick icon-master.png -thumbnail 128x128 -background transparent -gravity center -extent 128x128 icon-128.png
+magick icon-master.png -thumbnail 64x64 -background transparent -gravity center -extent 64x64 icon-64.png
+magick icon-master.png -thumbnail 48x48 -background transparent -gravity center -extent 48x48 icon-48.png
+magick icon-master.png -thumbnail 40x40 -background transparent -gravity center -extent 40x40 icon-40.png
+#magick icon-master.png -thumbnail 32x32 -background transparent -gravity center -extent 32x32 icon-32.png
+#magick icon-master.png -thumbnail 24x24 -background transparent -gravity center -extent 24x24 icon-24.png
+#magick icon-master.png -thumbnail 20x20 -background transparent -gravity center -extent 20x20 icon-20.png
+#magick icon-master.png -thumbnail 16x16 -background transparent -gravity center -extent 16x16 icon-16.png
 magick icon-256.png icon-64.png icon-48.png icon-40.png icon-32.png icon-24.png icon-20.png icon-16.png icon.ico
