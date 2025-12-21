58b3a150823472769fcceaf84e5e6c01cac3e133
Clownacy <Clownacy@users.noreply.github.com>
2025-12-21 01:37:51 +0000

    Qt: Deduplicate options-related code.
    Preprocessor macros suck...

--- qt/options.h
+++ qt/options.h
@@ -4,6 +4,7 @@
 #include <QSettings>
 
 #include "emulator.h"
+#include "paste.h"
 
 #define STRINGIFY(STRING) STRINGIFY_INTERNAL(STRING)
 #define STRINGIFY_INTERNAL(STRING) #STRING
@@ -28,13 +29,13 @@ public:
 		return VARIABLE; \
 	}
 
-#define DEFINE_OPTION_SETTER_UNSAVED(IDENTIFIER, VARIABLE) \
+#define DEFINE_UNSAVED_OPTION_SETTER(IDENTIFIER, VARIABLE) \
 	void IDENTIFIER(const auto value) \
 	{ \
 		VARIABLE = value; \
 	}
 
-#define DEFINE_OPTION_SETTER_SAVED(IDENTIFIER, VARIABLE) \
+#define DEFINE_SAVED_OPTION_SETTER(IDENTIFIER, VARIABLE) \
 	void IDENTIFIER(const auto value) \
 	{ \
 		VARIABLE = value; \
@@ -43,53 +44,63 @@ public:
 		settings.setValue(STRINGIFY(IDENTIFIER), value); \
 	}
 
-#define DEFINE_OPTION_UNSAVED(IDENTIFIER, VARIABLE) \
+#define DEFINE_UNSAVED_OPTION_GETTER_AND_SETTER(IDENTIFIER, VARIABLE) \
 	DEFINE_OPTION_GETTER(IDENTIFIER, VARIABLE) \
-	DEFINE_OPTION_SETTER_UNSAVED(IDENTIFIER, VARIABLE)
+	DEFINE_UNSAVED_OPTION_SETTER(IDENTIFIER, VARIABLE)
 
-#define DEFINE_OPTION_SAVED(IDENTIFIER, VARIABLE) \
+#define DEFINE_SAVED_OPTION_GETTER_AND_SETTER(IDENTIFIER, VARIABLE) \
 	DEFINE_OPTION_GETTER(IDENTIFIER, VARIABLE) \
-	DEFINE_OPTION_SETTER_SAVED(IDENTIFIER, VARIABLE)
+	DEFINE_SAVED_OPTION_SETTER(IDENTIFIER, VARIABLE)
 
-	DEFINE_OPTION_SAVED(TVStandard, emulator_configuration.general.tv_standard)
-	DEFINE_OPTION_SAVED(Region, emulator_configuration.general.region)
-	DEFINE_OPTION_SAVED(CDAddonEnabled, emulator_configuration.general.cd_add_on_enabled)
+#define DEFINE_UNSAVED_OPTIONS(...) \
+	PASTE2(DEFINE_UNSAVED_OPTION_GETTER_AND_SETTER, __VA_ARGS__)
 
-	DEFINE_OPTION_SAVED(IntegerScreenScalingEnabled, integer_screen_scaling)
-	DEFINE_OPTION_SAVED(TallInterlaceMode2Enabled, tall_interlace_mode_2)
-	DEFINE_OPTION_SAVED(WidescreenEnabled, emulator_configuration.vdp.widescreen_enabled)
+#define DEFINE_SAVED_OPTIONS(...) \
+	PASTE2(DEFINE_SAVED_OPTION_GETTER_AND_SETTER, __VA_ARGS__)
 
-	DEFINE_OPTION_SAVED(LowPassFilterDisabled, emulator_configuration.general.low_pass_filter_disabled)
-	DEFINE_OPTION_SAVED(LowVolumeDistortionDisabled, emulator_configuration.fm.ladder_effect_disabled)
+	DEFINE_SAVED_OPTIONS(
+		TVStandard, emulator_configuration.general.tv_standard,
+		Region, emulator_configuration.general.region,
+		CDAddonEnabled, emulator_configuration.general.cd_add_on_enabled,
 
-	DEFINE_OPTION_SAVED(KeyboardControlPad, keyboard_control_pad)
+		IntegerScreenScalingEnabled, integer_screen_scaling,
+		TallInterlaceMode2Enabled, tall_interlace_mode_2,
+		WidescreenEnabled, emulator_configuration.vdp.widescreen_enabled,
 
-	DEFINE_OPTION_UNSAVED(SpritesDisabled, emulator_configuration.vdp.sprites_disabled)
-	DEFINE_OPTION_UNSAVED(WindowPlaneDisabled, emulator_configuration.vdp.window_disabled)
-	DEFINE_OPTION_UNSAVED(ScrollPlaneADisabled, emulator_configuration.vdp.planes_disabled[0])
-	DEFINE_OPTION_UNSAVED(ScrollPlaneBDisabled, emulator_configuration.vdp.planes_disabled[1])
+		LowPassFilterDisabled, emulator_configuration.general.low_pass_filter_disabled,
+		LowVolumeDistortionDisabled, emulator_configuration.fm.ladder_effect_disabled,
 
-	DEFINE_OPTION_UNSAVED(FM1Disabled, emulator_configuration.fm.fm_channels_disabled[0])
-	DEFINE_OPTION_UNSAVED(FM2Disabled, emulator_configuration.fm.fm_channels_disabled[1])
-	DEFINE_OPTION_UNSAVED(FM3Disabled, emulator_configuration.fm.fm_channels_disabled[2])
-	DEFINE_OPTION_UNSAVED(FM4Disabled, emulator_configuration.fm.fm_channels_disabled[3])
-	DEFINE_OPTION_UNSAVED(FM5Disabled, emulator_configuration.fm.fm_channels_disabled[4])
-	DEFINE_OPTION_UNSAVED(FM6Disabled, emulator_configuration.fm.fm_channels_disabled[5])
-	DEFINE_OPTION_UNSAVED(DACDisabled, emulator_configuration.fm.dac_channel_disabled)
+		KeyboardControlPad, keyboard_control_pad
+	)
 
-	DEFINE_OPTION_UNSAVED(PSG1Disabled, emulator_configuration.psg.tone_disabled[0])
-	DEFINE_OPTION_UNSAVED(PSG2Disabled, emulator_configuration.psg.tone_disabled[1])
-	DEFINE_OPTION_UNSAVED(PSG3Disabled, emulator_configuration.psg.tone_disabled[2])
-	DEFINE_OPTION_UNSAVED(PSGNoiseDisabled, emulator_configuration.psg.noise_disabled)
+	DEFINE_UNSAVED_OPTIONS(
+		SpritesDisabled, emulator_configuration.vdp.sprites_disabled,
+		WindowPlaneDisabled, emulator_configuration.vdp.window_disabled,
+		ScrollPlaneADisabled, emulator_configuration.vdp.planes_disabled[0],
+		ScrollPlaneBDisabled, emulator_configuration.vdp.planes_disabled[1],
 
-	DEFINE_OPTION_UNSAVED(PCM1Disabled, emulator_configuration.pcm.channels_disabled[0])
-	DEFINE_OPTION_UNSAVED(PCM2Disabled, emulator_configuration.pcm.channels_disabled[1])
-	DEFINE_OPTION_UNSAVED(PCM3Disabled, emulator_configuration.pcm.channels_disabled[2])
-	DEFINE_OPTION_UNSAVED(PCM4Disabled, emulator_configuration.pcm.channels_disabled[3])
-	DEFINE_OPTION_UNSAVED(PCM5Disabled, emulator_configuration.pcm.channels_disabled[4])
-	DEFINE_OPTION_UNSAVED(PCM6Disabled, emulator_configuration.pcm.channels_disabled[5])
-	DEFINE_OPTION_UNSAVED(PCM7Disabled, emulator_configuration.pcm.channels_disabled[6])
-	DEFINE_OPTION_UNSAVED(PCM8Disabled, emulator_configuration.pcm.channels_disabled[7])
+		FM1Disabled, emulator_configuration.fm.fm_channels_disabled[0],
+		FM2Disabled, emulator_configuration.fm.fm_channels_disabled[1],
+		FM3Disabled, emulator_configuration.fm.fm_channels_disabled[2],
+		FM4Disabled, emulator_configuration.fm.fm_channels_disabled[3],
+		FM5Disabled, emulator_configuration.fm.fm_channels_disabled[4],
+		FM6Disabled, emulator_configuration.fm.fm_channels_disabled[5],
+		DACDisabled, emulator_configuration.fm.dac_channel_disabled,
+
+		PSG1Disabled, emulator_configuration.psg.tone_disabled[0],
+		PSG2Disabled, emulator_configuration.psg.tone_disabled[1],
+		PSG3Disabled, emulator_configuration.psg.tone_disabled[2],
+		PSGNoiseDisabled, emulator_configuration.psg.noise_disabled,
+
+		PCM1Disabled, emulator_configuration.pcm.channels_disabled[0],
+		PCM2Disabled, emulator_configuration.pcm.channels_disabled[1],
+		PCM3Disabled, emulator_configuration.pcm.channels_disabled[2],
+		PCM4Disabled, emulator_configuration.pcm.channels_disabled[3],
+		PCM5Disabled, emulator_configuration.pcm.channels_disabled[4],
+		PCM6Disabled, emulator_configuration.pcm.channels_disabled[5],
+		PCM7Disabled, emulator_configuration.pcm.channels_disabled[6],
+		PCM8Disabled, emulator_configuration.pcm.channels_disabled[7]
+	)
 };
 
 #endif // OPTIONS_H
