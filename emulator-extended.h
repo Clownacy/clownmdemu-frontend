d456ead3e31636a09400523c8653e9d067d24a0c
Clownacy <Clownacy@users.noreply.github.com>
2025-12-23 14:02:33 +0000

    Make EmulatorExtended class header-only.
    Makes it easier to convert to a template in a future commit.

--- emulator-extended.h
+++ emulator-extended.h
@@ -14,15 +14,60 @@ protected:
 	CDReader cd_reader;
 	AudioOutput audio_output;
 
-	void FMAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_fm_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final;
-	void PSGAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_psg_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final;
-	void PCMAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_pcm_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final;
-	void CDDAAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_cdda_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final;
+	void FMAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_fm_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final
+	{
+		generate_fm_audio(clownmdemu, audio_output.MixerAllocateFMSamples(total_frames), total_frames);
+	}
+	void PSGAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_psg_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final
+	{
+		generate_psg_audio(clownmdemu, audio_output.MixerAllocatePSGSamples(total_frames), total_frames);
+	}
+	void PCMAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_pcm_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final
+	{
+		generate_pcm_audio(clownmdemu, audio_output.MixerAllocatePCMSamples(total_frames), total_frames);
+	}
+	void CDDAAudioToBeGenerated(const ClownMDEmu *clownmdemu, std::size_t total_frames, void (*generate_cdda_audio)(const ClownMDEmu *clownmdemu, cc_s16l *sample_buffer, std::size_t total_frames)) override final
+	{
+		generate_cdda_audio(clownmdemu, audio_output.MixerAllocateCDDASamples(total_frames), total_frames);
+	}
 
-	void CDSeeked(cc_u32f sector_index) override final;
-	void CDSectorRead(cc_u16l *buffer) override final;
-	cc_bool CDTrackSeeked(cc_u16f track_index, ClownMDEmu_CDDAMode mode) override final;
-	std::size_t CDAudioRead(cc_s16l *sample_buffer, std::size_t total_frames) override final;
+	void CDSeeked(cc_u32f sector_index) override final
+	{
+		cd_reader.SeekToSector(sector_index);
+	}
+	void CDSectorRead(cc_u16l *buffer) override final
+	{
+		cd_reader.ReadSector(buffer);
+	}
+	cc_bool CDTrackSeeked(cc_u16f track_index, ClownMDEmu_CDDAMode mode) override final
+	{
+		CDReader::PlaybackSetting playback_setting;
+
+		switch (mode)
+		{
+			default:
+				assert(false);
+				return cc_false;
+
+			case CLOWNMDEMU_CDDA_PLAY_ALL:
+				playback_setting = CDReader::PlaybackSetting::ALL;
+				break;
+
+			case CLOWNMDEMU_CDDA_PLAY_ONCE:
+				playback_setting = CDReader::PlaybackSetting::ONCE;
+				break;
+
+			case CLOWNMDEMU_CDDA_PLAY_REPEAT:
+				playback_setting = CDReader::PlaybackSetting::REPEAT;
+				break;
+		}
+
+		return cd_reader.PlayAudio(track_index, playback_setting);
+	}
+	std::size_t CDAudioRead(cc_s16l *sample_buffer, std::size_t total_frames) override final
+	{
+		return cd_reader.ReadAudio(sample_buffer, total_frames);
+	}
 
 public:
 	struct State
@@ -81,7 +126,16 @@ public:
 		Emulator::HardReset(IsCDInserted());
 	}
 
-	void Iterate();
+	void Iterate()
+	{
+		// Reset the audio buffers so that they can be mixed into.
+		audio_output.MixerBegin();
+
+		Emulator::Iterate();
+
+		// Resample, mix, and output the audio for this frame.
+		audio_output.MixerEnd();
+	}
 
 	// TODO: Make Qt frontend use this!!!
 	void SetAudioPALMode(const bool enabled)
