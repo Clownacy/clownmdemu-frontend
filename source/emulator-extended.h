78b6c90a06cd34c27c9d2238818edba285d54ef2
Clownacy <Clownacy@users.noreply.github.com>
2026-03-30 17:39:11 +0100

    Fix save data being lost when hard-resetting.
    I swear that this is the third time I have fixed this exact bug.

--- source/emulator-extended.h
+++ source/emulator-extended.h
@@ -264,22 +264,29 @@ private:
 
 	void LoadCartridgeSaveData(const std::filesystem::path &cartridge_file_path)
 	{
-		cartridge_save_file_path = save_file_directory / cartridge_file_path.stem().replace_extension(".srm");
-
 		// Reset external RAM to its default 'empty' state.
 		// This way, even if a file is not loaded, the buffer is at least initialised to something.
 		auto &external_ram_buffer = this->GetExternalRAM();
 		external_ram_buffer.fill(0xFF);
 
-		// Load save data from disk.
-		if (std::filesystem::exists(cartridge_save_file_path))
+		if (cartridge_file_path.empty())
 		{
-			const auto save_data_size = std::filesystem::file_size(cartridge_save_file_path);
+			cartridge_save_file_path.clear();
+		}
+		else
+		{
+			cartridge_save_file_path = save_file_directory / cartridge_file_path.stem().replace_extension(".srm");
 
-			if (save_data_size > std::size(external_ram_buffer))
-				Frontend::debug_log.Log("Save data file size (0x{:X} bytes) is larger than the internal save data buffer size (0x{:X} bytes)", save_data_size, std::size(external_ram_buffer));
-			else
-				std::ifstream(cartridge_save_file_path, std::ios::binary).read(reinterpret_cast<char*>(std::data(external_ram_buffer)), save_data_size);
+			// Load save data from disk.
+			if (std::filesystem::exists(cartridge_save_file_path))
+			{
+				const auto save_data_size = std::filesystem::file_size(cartridge_save_file_path);
+
+				if (save_data_size > std::size(external_ram_buffer))
+					Frontend::debug_log.Log("Save data file size (0x{:X} bytes) is larger than the internal save data buffer size (0x{:X} bytes)", save_data_size, std::size(external_ram_buffer));
+				else
+					std::ifstream(cartridge_save_file_path, std::ios::binary).read(reinterpret_cast<char*>(std::data(external_ram_buffer)), save_data_size);
+			}
 		}
 	}
 
@@ -416,6 +423,8 @@ public:
 		// Reset state buffer, since it cannot undo the cartridge or CD being changed.
 		state_rewind_buffer.Clear();
 
+		SaveCartridgeSaveData();
+
 		cartridge_buffer = buffer;
 		Emulator::InsertCartridge(buffer, buffer_length);
 
@@ -423,25 +432,15 @@ public:
 
 		LoadCartridgeSaveData(cartridge_file_path);
 
-		HardReset();
+		if (this->IsCartridgeInserted() || IsCDInserted())
+			HardReset();
 
 		UpdateTitle();
 	}
 
-	template<typename... Ts>
-	void EjectCartridge(Ts &&...args)
+	void EjectCartridge()
 	{
-		state_rewind_buffer.Clear();
-
-		SaveCartridgeSaveData();
-		cartridge_save_file_path.clear();
-		cartridge_buffer = nullptr;
-		Emulator::EjectCartridge(std::forward<Ts>(args)...);
-
-		if (IsCDInserted())
-			HardReset();
-
-		UpdateTitle();
+		InsertCartridge({}, nullptr, 0);
 	}
 
 	////////
