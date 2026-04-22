fe293cd358227ebe1e7239d936021d59ece86c29
Clownacy <Clownacy@users.noreply.github.com>
2026-04-22 12:44:13 +0100

    Clean-up and fix the debug logging code.
    Now prints to the console properly again.

--- source/debug-log.cpp
+++ source/debug-log.cpp
@@ -8,14 +8,14 @@ void DebugLog::Log(std::string message)
 	{
 		try
 		{
-			lines.emplace_back(std::move(message));
-
 			if (log_to_console || force_console_output)
-				SDL_LogMessage(SDL_LOG_CATEGORY_ERROR, SDL_LOG_PRIORITY_ERROR, "%s", message.c_str());
+				SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_WARN, "%s", message.c_str());
+
+			lines.emplace_back(std::move(message));
 		}
 		catch (const std::bad_alloc&)
 		{
-			// Wipe the line buffer to reclaim RAM. It's better than nothing.
+			// Wipe the line buffer to reclaim RAM; it's better than nothing.
 			lines.clear();
 		}
 	}
@@ -23,23 +23,18 @@ void DebugLog::Log(std::string message)
 
 void DebugLog::Log(const char* const format, std::va_list args)
 {
-	const auto GetSize = [&]()
+	const auto FormatToString = [](const char* const format, std::va_list args)
 	{
+		std::string message_string;
+
 		std::va_list args2;
 		va_copy(args2, args);
-		const std::size_t message_buffer_size = static_cast<std::size_t>(SDL_vsnprintf(nullptr, 0, format, args2));
+		message_string.resize(static_cast<std::size_t>(SDL_vsnprintf(nullptr, 0, format, args2)));
 		va_end(args2);
-		return message_buffer_size;
+
+		SDL_vsnprintf(std::data(message_string), std::size(message_string) + 1, format, args);
+		return message_string;
 	};
 
-	const auto WriteBuffer = [&](char* const message_buffer, const std::size_t message_buffer_size)
-	{
-		SDL_vsnprintf(message_buffer, message_buffer_size + 1, format, args);
-	};
-
-	std::string message_string;
-	message_string.resize(GetSize());
-	WriteBuffer(std::data(message_string), std::size(message_string));
-
-	Log(std::move(message_string));
+	Log(FormatToString(format, args));
 }
